b1b4d04de7c4db1e8322a049f53696879400efe4
Tobi <tobi.akiny@gmail.com>
2016-09-25 00:22:40 +0100

    .

--- GA.c
+++ GA.c
@@ -33,46 +33,50 @@ typedef struct  {
     int fitness;
 } gen;
 
+char randchar(){
+	return rand()%27 ? 'a'+(random() % 26):' ';
+}
+
+char *randstr(int len) {
+	char randStr[17]; 
+	while (len--) randStr[len] = randchar();
+	return (char*) &randStr;
+}
+
 int main() {
-	char[] sentence = "generated string";
+	srand(get_time()*10000);
 	int noMatch = 1;
 	gen top[2];
 	gen generated[] = {
-		{ "sjfdaospfjapofja", 0 },
-		{ "iqweuqwioruiqwor", 0 },
-		{ "zxnmcrznxmnvxmcc", 0 },
-		{ "wqcnioenfgjnlwoz", 0 }
+		{ randstr(16), 0 },
+		{ randstr(16), 0 },
+		{ randstr(16), 0 },
+		{ randstr(16), 0 }
 	};
+	char *sentence = "generated string";
 	double start = get_time();
-	srand(get_time()*10000);
-
 	while (noMatch) {
 		for (int i = 4; i--;) {
-			gen* GEN = &generated[i];
+			gen *GEN = &generated[i];
 			GEN->fitness=0;
-	
-			for (int j = 16; j--;) if (GEN->string[j]=="generated string"[j]) {
-				GEN->fitness++;
-			}
-			if (GEN->fitness>15) {
-				noMatch = 0;
-				break;
-			}
+			for (int j = 16; j--;) if (GEN->string[j]==sentence[j]) GEN->fitness++;
+			for (int x=0; x < 16; ++x) printf("%c",generated[i].string[x]);
+			printf(" %d\n",GEN->fitness);
+			if (GEN->fitness>15) goto end;
 		}
-		for (int j = 4; j--;) {
-			if (generated[j].fitness > top[1].fitness || !top[1].fitness) {
-				if (generated[j].fitness > top[0].fitness) top[0] = generated[j];
-				else top[1] = generated[j];
-			}
+		for (int j = 4; j--;) if (generated[j].fitness > top[1].fitness || !top[1].fitness) {
+			gen *GEN = &generated[j];
+			if (GEN->fitness > top[0].fitness) top[0] = *GEN;
+			else top[1] = *GEN;
 		}
-		for (int j = 4; j--;) {
-			int IND = rand()%16;	
-			for (int z = 16; z--;) generated[j].string[z] = top[rand()%2].string[z];
-			printf("%s : %d\n",generated[j].string,generated[j].fitness);
-			if (!(rand()%100)) generated[j].string[IND] = rand()%27 ? 'a'+(random() % 26):' ';
+		for (int j = 4; j--;) for (int z = 16; z--;) {
+			gen *GEN = &generated[j];
+			if (!(rand()%50)) GEN->string[z] = randchar();
+			else GEN->string[z] = top[rand()%2].string[z];
 		}
 	}
-	printf("\nMatched \"generated string\" in %f seconds\n",get_time()-start);
+	end:
+	printf("\nMatched \"%s\" in %f seconds\n",sentence,get_time()-start);
 	return 0;
 }
 
